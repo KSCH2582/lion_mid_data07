cb91e4c5fd2fc54c048889f53f516aa865862b3e
yujin01-s <snam6879@naver.com>
2025-05-29 15:46:18 +0900

    Fixed: 측정시간대 별 음주농도량 추이분석(1)

--- yujin_mid_project.py
+++ yujin_mid_project.py
@@ -2,16 +2,7 @@
 # 아침에 적발되는 알코올 측정 농도 확인
 # 2020년 ~ 2023년 측정 시간대별로 알코올 농도량 추이 분석
 
-# 자동으로 파일 목록 수집
-# %% 데이터 불러오기
-import glob
-
-all_csv_files = glob.glob("lion_mid_data07/data/*.csv")
-
-# %% 연도 필터링
-filtered_files = [file for file in all_csv_files if any(year in file for year in ["2020", "2021", "2022", "2023"])]
-
-# %% 확인
-for file in filtered_files:
-    print(file)
+# 먼저 파일의 경로를 확인한다.
+import os
+print(os.getcwd())
 
